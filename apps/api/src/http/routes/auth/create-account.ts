e2156ae389bf67c37da283c51550bffaf4a492b0
josuellions <josuellions@gmail.com>
2026-05-08 14:09:46 -0300

    feat: create `user` route structure and connection to `database`.

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
+import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
+
+import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app:FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/users', {
@@ -12,8 +14,17 @@ export async function createAccount(app:FastifyInstance) {
 
       })
     }
-  }, (req, res) => {
+  }, async (req, reply) => {
     const {name, email, password } = req.body;
-    return {message:'User created', response: {name, email, password}};
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    if(!userWithSameEmail) {
+      return reply.status(400).send({message: 'use with same e-mail alredy exists.'})
+    }
+
+    return reply.status(200).send(userWithSameEmail)
   })
 }
