fb18dd70f132be9c1a38193759303b72bbb58d75
josuellions <josuellions@gmail.com>
2026-05-12 16:49:55 -0300

    feat: create page auth forgot-password `login`.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,10 @@
 import AuthLayout  from "../app/auth/layout";
 import SingInPage from "./auth/sign-in/page";
-import SingUpPage from "./auth/sign-up/page";
 
 export default function Home() {
   return (
      <AuthLayout>
-      
-        {/* <SingInPage/> */}
-        <SingUpPage/>
+        <SingInPage/>
      </AuthLayout>
   )
 }
