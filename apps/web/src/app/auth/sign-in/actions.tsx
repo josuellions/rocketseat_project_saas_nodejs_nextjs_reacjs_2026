2b68a710cdf213e7a1d7374cbbeb36c54f6b6351
josuellions <josuellions@gmail.com>
2026-05-12 18:07:08 -0300

    feat: create request `ky` to `auth` login.

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,6 +1,15 @@
 'use server'
 
+import { singInWithPassword } from "@/http/sing-in-with-password";
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.info(">>DATA FORM ACTION")
-  console.log(Object.fromEntries(data))
+ 
+  const {email, password } = Object.fromEntries(data);
+
+  const result = await singInWithPassword({
+    email: String(email),
+    password: String(password)
+  })
+
+  console.log(result)
 }
