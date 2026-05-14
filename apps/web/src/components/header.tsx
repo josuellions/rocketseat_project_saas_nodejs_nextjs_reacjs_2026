cf9eab97689966ec27d7215940402a9ca7b693f3
josuellions <josuellions@gmail.com>
2026-05-14 11:38:26 -0300

    feat: creates the component `header-organization` in `web`.

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,12 +2,19 @@ import Image from "next/image"
 
 import logo from "@/assets/img/logos/logo_01.png"
 import { ProfileButton } from "./profile-button"
+import { OrganizationSwitcher } from "./organization-switcher"
+import { Slash } from "lucide-react"
 
 export function Header() {
   return (
     <div className="w-full flex mx-auto max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
-       <Image src={logo} alt="logo project manager" className="object-cover object-center w-16 h-16 rounded-full" priority/>
+        <Image src={logo} alt="logo project manager" className="object-cover object-center w-16 h-16 rounded-full" priority/>
+      
+        <Slash className="size-4 -rotate-[24deg] text-border"/>
+
+        <OrganizationSwitcher/>
+
       </div>
       <div className="flex items-center gap-4">
         <ProfileButton/>
