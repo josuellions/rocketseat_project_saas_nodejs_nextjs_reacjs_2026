d1ea26abcf35c6178547573b5de2403b3c537e8c
josuellions <josuellions@gmail.com>
2026-05-14 22:26:21 -0300

    build: configures `web` environment variables.

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -11,14 +11,21 @@ export const env = createEnv({
     SERVER_PORT: z.coerce.number().default(3333),
   },
   client: {},
-  shared: {},
+  shared: {
+    NEXT_COOKIE_TOKEN: z.string(),
+    NEXT_COOKIE_ORGANIZATION: z.string(),
+    NEXT_PUBLIC_API_URL: z.url().default("http://localhost:3333")
+  },
   runtimeEnv: {
     SERVER_PORT: process.env.SERVER_PORT,
     JWT_SECRET:  process.env.JWT_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     GTIHUB_OAUTH_CLIENT_ID: process.env.GTIHUB_OAUTH_CLIENT_ID,
     GTIHUB_OAUTH_CLIENT_SECRET: process.env.GTIHUB_OAUTH_CLIENT_SECRET,
     GTIHUB_OAUTH_CLIENT_REDIRECT_URI: process.env.GTIHUB_OAUTH_CLIENT_REDIRECT_URI,
+    NEXT_COOKIE_ORGANIZATION: process.env.NEXT_COOKIE_ORGANIZATION,
+    NEXT_COOKIE_TOKEN: process.env.NEXT_COOKIE_TOKEN
   },
   emptyStringAsUndefined: true,
 })
